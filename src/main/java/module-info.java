4beb79bbb53a46b188d4d04990c663bd4f3e524f
Nana-Boaten <gooddevil273@gmail.com>
2025-08-04 18:07:40 +0000

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.sell_all {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.sell_all to javafx.fxml;
+    exports com.example.sell_all;
+}
